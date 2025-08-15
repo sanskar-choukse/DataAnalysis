a12a620765d0d23eeac88cdaeb5cfcd2a21d9e2e
Sanskar Chouksey <146567737+sanskar-choukse@users.noreply.github.com>
2025-08-15 13:45:40 +0530

    Delete SQL  basics query directory

--- SQL  basics query/19-Group By SQL Example Server.sql
+++
@@ -1,8 +0,0 @@
-select * from dbo.sales
-
-select (Paymentmethod),sum(TotalAmount) from dbo.sales
-group by PaymentMethod
-
-select ProductID,Paymentmethod,sum(TotalAmount) as [sum of Sales]  from dbo.sales
-group by ProductID,Paymentmethod 
-order by ProductID
