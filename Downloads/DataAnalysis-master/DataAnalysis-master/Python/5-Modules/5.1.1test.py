a5af5c81d010a673fa978a9675bf22c01827da6c
sanskar chouksey <sanskarchoukse468@gmail.com>
2025-11-03 21:32:13 +0530

    cleaned the repo unnecessary files

--- Downloads/DataAnalysis-master/DataAnalysis-master/Python/5-Modules/5.1.1test.py
+++
@@ -1,5 +0,0 @@
-from package.maths import *
-# from package.subpackage.mult import multiply
-print(addition(2,3))
-print(subtract(8,3))
-# print(multiply(2,3))
--- Downloads/DataAnalysis-master/DataAnalysis-master/Python/5-Modules/package/__init__.py
+++
@@ -1,3 +0,0 @@
-'''__init__.py is a special file used in python to define 
-packages and initalize their namespace
-'''
